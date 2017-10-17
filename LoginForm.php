eb81ec572b4aacee09ddd83a6b2ad54d0403554b
s1551396 <32844938+s1551396@users.noreply.github.com>
2017-10-17 16:51:58 +0200

    Add files via upload
    Excuses. In feite brengt de login pagina je meteen naar de login-succesvol pagina onafhankelijk van wat er in de login ingetypt is. Zet beide bestanden in dezelfde map en pas het pad van LoginForm naar login_succesful aan en het zou moeten werken. Ik probeer nog een login formaat te implementeren van
    http://form.guide/php-form/php-login-form.html
    en
    http://form.guide/php-form/php-registration-form.html
    maar omdat een sql database implementeren nog moeilijk gaat, is me dat nog niet gelukt.
    
    Overigens staan deze twee pagina's online op:
    http://liacs.leidenuniv.nl/~s1551396/LoginForm.php
    
    Ik ga morgen nog kijken of ik het zo kan instellen dat je alleen met een bepaalde username+wachtwoord combinatie naar de login_succesful pagina wordt doorgestuurd. Maar voel je vrij om de interface kleuren te geven.

---
+++ LoginForm.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+<body>
+
+  <h1>Login pagina</h1>
+  
+  <form action="login_succesful.php" method="post">
+    username:
+    <input type="text"><br/>
+    password:
+    <input type="text"><br/>
+    <input type="submit" value="Login">
+  </form>
+
+</body>
+</html>
---
+++ login_succesful.php
@@ -0,0 +1,8 @@
+ <!DOCTYPE html>
+<html lang="en-UK">
+<body>
+
+<p>U bent ingelogd.</p>
+
+</body>
+</html> 
