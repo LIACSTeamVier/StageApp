c9f435a4369adc5da76db4b93172ba83450bb88b
s1551396 <32844938+s1551396@users.noreply.github.com>
2017-10-24 15:35:02 +0200

    Add files via upload
    De te versturen email aan een stagebegeleider die een account gekregen heeft. Ik heb dit nog niet werkend gekregen met attempting_to_email.php

---
+++ email.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+<body>
+
+
+  <p>Dear <?php$name?>,</p><br/>
+  <p>An account has been made for you on the <a href='http://liacs.leidenuniv.nl/~s1551396/StageApp/Login.php'>LIACS InternshipApp</a>. Your username and password are as follows:<p><br/>
+  <p>Username: <?php$email?></p><br/>
+  <p>Password: <?php$password?></p><br/>
+  <p>Please do not reply to this e-mail.</p><br/>
+
+
+</body>
+</html> 
