adb3839ec2fade474059666d1007473080289988
s1551396 <32844938+s1551396@users.noreply.github.com>
2017-10-24 15:24:20 +0200

    Add files via upload
    Admin heeft een extra drop-down menu

--- main_page.php
+++ main_page.php
@@ -1,13 +1,46 @@
- <!DOCTYPE html>
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
 <html lang="en-UK">
 <body>
 
-<?php
-  $username = $_POST["username"];
-  echo "<h1>Welkom " . "$username" . "</h1>";
-?>
+  <?php
+    if (!isset($_SESSION["username"]) || empty($_SESSION["username"])) {
+      header("Location: http://liacs.leidenuniv.nl/~s1551396/StageApp/Login.php");
+      exit;
+      // TODO, send info of expired session to login page.
+      // TODO, expire session after period of inactivity.
+    }
+    $username = $_SESSION["username"];
+    $class = $_SESSION["class"];
+    echo "<h1>Welcome " . "$username" . "." ."</h1>";
+    echo "<p>You are a(n) " . "$class" . "." ."<p>"; //TODO temp, remove line.
+    
+    if ($class == "Admin") {
+      //Creating an account
+        echo "
+          <p> Create an account </p>
+          <select name='createAcc' onchange='location = this.value;'>
+            <option disabled selected value> -- Select an option -- </option>
+            <option value='create_acc/admin_account.php'>Admin</option>
+            <option value='create_acc/intern_instruc_account.php'>Internship instructor</option>
+            <option value='create_acc/instruc_account.php'>Instructor</option>
+            <option value='create_acc/Student_account.php'>Student</option>
+          </select>";
+    
+    
+    
+    
+    }
+    
+    
+  ?>
 
-<p>U bent ingelogd.</p>
+  <form action="attempting_Logout.php" method="post">
+    <input type="submit" value="Logout">
+  </form>
 
 </body>
 </html> 
