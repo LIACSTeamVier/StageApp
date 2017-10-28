a0732a9e119d34410cff66a8de354f7e86db40c3
MartenLiacs <32845237+MartenLiacs@users.noreply.github.com>
2017-10-28 20:18:47 +0000

    verandering gemaakt gebaseerd op feedback en database design

--- stage.php
+++ stage.php
@@ -1,158 +1,241 @@
 <?php //file made with help from w3schools.com
+    session_start();
+    //TODO put stuff in sessions when logging in, fix the vars in here to match the session, fix the stuff in here to match the correct database, and fix to match the table
+    ///!!!! put correct stuff in the session
 
-session_start();
+    $locationErr = $streetErr = $streetnrErr = $payErr = $tnotesErr = $naamErr = $tijdrestErr = $squalErr = $descriptionErr = "";
+    $location = $street = $streetnr = $pay = $travel = $tnotes = $description = $tijdrest = $naam = $squal = "";
 
-//TODO put stuff in sessions when logging in, fix the vars in here to match the session, fix the stuff in here to match the correct database, and fix to match the table
-
-///!!!! put correct stuff in the session
-//echo "Hello " . $_SESSION["name"] . " " . $_SESSION["surname"]. ".<br>";
-echo "Hello " . $_SESSION["uname"] . ".<br>";
-//test if the user is allowed to make a project   TODO put correct vars in session and check the correct values
-if (($_SESSION["class"] != "Admin") && ($_SESSION["class"] != "stagebegeleider")){
-	//redirect to main page
-	header("Location: main_page.php");
-	die();
-}
-else{
-// define variables and set to empty values
-$nameErr = $emailErr = $teleErr = $descriptionErr = "";
-$name = $email = $tele = $location = $company = $description = "";
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Name is required";
-    } else {
-        $name = test_input($_POST["name"]);
+    //test if the user is allowed to make a project   TODO put correct vars in session and check the correct values
+    if (($_SESSION["class"] != "Admin") && ($_SESSION["class"] != "stagebegeleider")){
+        //redirect to main page
+        header("Location: main_page.php");
+        die();
     }
+    else{
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $con = mysqli_connect("mysql.liacs.leidenuniv.nl", "s1553968", "-", "s1553968");
+            if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            }
+        
+            if (empty($_POST["naam"])) {
+                $naamErr = "Name is required";
+            } else {
+                $naam = test_input($_POST["naam"]);
+                if(strlen($naam) > 30)
+                    $naamErr = "Input too big";
 
-    if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-    } else {
-        $email = test_input($_POST["email"]);
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          $emailErr = "Invalid email format";
+                //test if name is taken
+                $stmt = mysqli_prepare($con, "SELECT * FROM Project p WHERE p.ProjectNaam = ?");
+                mysqli_stmt_bind_param($stmt,'s', $naam);
+                mysqli_stmt_execute($stmt);
+                $result = mysqli_stmt_get_result($stmt);
+                mysqli_stmt_close($stmt);
+                if (!$result){
+                    echo "database error!";
+                    die ('Unable to run query:' . mysqli_error());
+                }
+                else
+                    $row = mysqli_fetch_row($result);
+                if(!empty($row))
+                    $naamErr = "Name is taken";
+            }
+
+            if (empty($_POST["location"])) {
+                $locationErr = "Location is required";
+            } else {
+                $location = test_input($_POST["location"]); 
+                if(strlen($location) > 30)
+                    $locationErr = "Input too big";
+            }
+
+            if(empty($_POST["street"])){
+                $street = "";
+            }else{
+                $street = test_input($_POST["street"]);
+                if(strlen($street) > 30)
+                    $streetErr = "Input too big";
+            }
+
+            if(empty($_POST["streetnr"])){
+                $streetnr = "";
+            }else{
+                $streetnr = test_input($_POST["streetnr"]);
+                if(strlen($streetnr) > 30)
+                    $streetnrErr = "Input too big";
+            }
+
+            if(empty($_POST["pay"])){
+                $pay = "";
+            }else{
+                $pay = test_input($_POST["pay"]);
+                if(strlen($pay) > 30)
+                    $payErr = "Input too big";
+            }
+
+            if(empty($_POST["tnotes"])){
+                $tnotes = "";
+            }else{
+                $tnotes = test_input($_POST["tnotes"]);
+                if(strlen($tnotes) > 30)
+                    $tnotesErr = "Input too big";
+            }
+            if(empty($_POST["travel"])){
+                $travel = "";
+            }else{
+                $travel =  test_input($_POST["travel"]);
+            }
+
+
+            if (empty($_POST["description"])) {
+                $descriptionErr = "A description of your internship is required";
+            } else {
+                $description = test_input($_POST["description"]);
+            }
+
+            if(empty($_POST["squal"])){
+                $squal = "";
+            }else{
+                $squal =  test_input($_POST["squal"]);
+            }
+            if(empty($_POST["tijdrest"])){
+                $tijdrest = "";
+            }else{
+                $tijdrest =  test_input($_POST["tijdrest"]);
+            }
+
+            if ( ($naamErr == "") && ($locationErr == "") && ($streetErr == "") && ($streetnrErr == "") && ($payErr == "")  && ($tnotesErr == "")
+                && ($descriptionErr =="") && ($squalErr == "") && ($tijdrestErr == "") ){
+                insertIntoDatabase($naam, $location, $street, $streetnr, $pay, $travel, $tnotes, $description, $squal, $tijdrest, $con);
+            }
+            else
+                mysqli_close($con);
         }
-    }
-
-    if (empty($_POST["tele"])) {
-        $tele = "";
-    } else {
-        $tele = test_input($_POST["tele"]); 
-        if (!preg_match("/^[0-9 ]*$/",$tele)) {
-              $teleErr = "Only numbers allowed";
-        }       
-    }
-
-    if (empty($_POST["location"])) {
-        $location = "";
-    } else {
-        $location = test_input($_POST["location"]); 
-    }
-
-    if (empty($_POST["company"])) {
-        $company = "";
-    } else {
-        $company = test_input($_POST["company"]);
-    }
+    } //BRACKET FOR SESSION ROLE CHECK!
     
-    if (empty($_POST["description"])) {
-        $descriptionErr = "A description of your internship is required";
-    } else {
-        $description = test_input($_POST["description"]);
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
+    function insertIntoDatabase($naam, $location, $street, $streetnr, $pay, $travel, $tnotes, $description, $squal, $tijdrest, $con){
+        $intsupname = $_SESSION["username"];//has to be the same name as the name in the stagebegeleider table
+        $stmt1 = mysqli_prepare($con, "SELECT BedrijfNaam FROM Stagebegeleider s WHERE s.SBegeleiderNaam = ?");
+        mysqli_stmt_bind_param($stmt1,'s', $intsupname);
+        mysqli_stmt_execute($stmt1);
+        $result = mysqli_stmt_get_result($stmt1);
+        mysqli_stmt_close($stmt1);
+        if (!$result){
+            echo "database error!";
+            die ('Unable to run query:' . mysqli_error());
+        }
+        else{
+            $row = mysqli_fetch_row($result);
+            $compname = $row[0];
+        }
+        if(empty($compname)){
+            //change error message in future
+            die("no company name linked to this account");
+            //	header("Location: main_page.php");
+        }	
+
+        $stmt2 = mysqli_prepare($con, "INSERT INTO Project(ProjectNaam, Beschrijving, Tijd) VALUES (?,?,?)");
+        mysqli_stmt_bind_param($stmt2, 'sss', $naam, $description, $tijdrest);
+        $result2 = mysqli_stmt_execute($stmt2);
+        //$result2 = mysqli_stmt_get_result($stmt2);
+        mysqli_stmt_close($stmt2);
+        if (!$result2){
+            //        echo "database error!";
+            header("Location: ".$_SERVER["PHP_SELF"]);
+            die('Unable to run query:' . mysqli_error() );
+        }
 
 
+        $stmt4 = mysqli_prepare($con, "INSERT INTO Stageplek_van(ProjectNaam, PlekNaam, Locatie, StraatNr, Travel, Tnotes,
+                                 Pay, Studentqualities, BedrijfNaam) VALUES (?,?,?,?,?,?,?,?,?)");
+        mysqli_stmt_bind_param($stmt4,'sssssssss', $naam, $location, $street, $streetnr, $travel, $tnotes, $pay, $squal, $compname);
+        $result4 = mysqli_stmt_execute($stmt4);
+        mysqli_stmt_close($stmt4);
+        if (!$result4){
+            header("Location: ".$_SERVER["PHP_SELF"]);
+            die('Unable to run query:' . mysqli_error() );
+        }
 
-    if ( ($nameErr == "") && ($emailErr == "") && ($teleErr == "")
-	 && ($descriptionErr =="")){
-        insertIntoDatabase($name, $email, $tele, $location, $company, $description);echo "well done proud of you bby";
+
+        $stmt3 = mysqli_prepare($con, "INSERT INTO Beslaat(ProjectNaam, BedrijfNaam) VALUES(?,?)");
+        mysqli_stmt_bind_param($stmt3,'ss', $naam, $compname);
+        $result3 = mysqli_stmt_execute($stmt3);
+        mysqli_stmt_close($stmt3);
+        if (!$result3){
+            header("Location: ".$_SERVER["PHP_SELF"]);
+            die('Unable to run query:' . mysqli_error() );	
+        }
+
+        mysqli_close($con);
+        //redirect to main page
+        header("Location: main_page.php");
+        die();
     }
-}
-} //BRACKET FOR SESSION ROLE CHECK!
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
-
-function insertIntoDatabase($name, $email, $tele, $location, $company, $description){
-    $con = mysqli_connect("mysql.liacs.leidenuniv.nl", "s1553968", "-", "s1553968");
-    ///change into this maybe later $con = mysqli_connect("mysql.liacs.leidenuniv.nl", "s1551396", "-", "s1551396");
-    // Check connection
-    if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
-/*    $statement = "INSERT INTO Stage (name, email, phone, location, company, description) VALUES ('$name', '$email','$tele','$location','$company','$description')";
-    $qr = "INSERT INTO Stage (name, email, phone, location, company, description) VALUES ('test4', 'testmail', '012344', 'test', 'test', 'testestststeste')";
-    $result = mysqli_query($con, $statement);
-//or die('Unable to run query:' . mysqli_error());*/
-
-    $stmt = mysqli_prepare($con, "INSERT INTO Stage (name, email, phone, location, company, description) VALUES (?,?,?,?,?,?)");
-    //change type of parameters in future if needed for db
-    mysqli_stmt_bind_param($stmt, 'ssssss', $name, $email, $tele, $location, $company, $description);
-    //check if true or false
-    $result = mysqli_stmt_execute($stmt);
-
-    if (!$result){
-	echo "database error!";
-    	die ('Unable to run query:' . mysqli_error());
-    }
-    mysqli_close($con);
-
-    //redirect to main page
-    header("Location: main_page.php");
-    die();
-}
-
 ?>
 <!doctype html>
 
 <html>
-        <head>
+    <head>
         <meta charset="utf-8" /> 
-          
+
         <meta name="Description" content= "Page To Offer Internships" />
         <link rel="stylesheet" type="text/css" href="style.css">
         <title>Page To Offer Internships</title>
     </head>
-   <body>
-     
-   <div class="sidepane">
-     <a href="#">Overview</a>
-     <a href="#">Projects</a>
-     <a href="#">Contact</a>
-     <a href="#">Help</a></a>
-   </div>
-     
-   <div class="main">
-    <p>
-        Fill in the forms to make your internship information available to the
-        students.
-    </p>
-    <p><span class="error">* required field.</span></p>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" id="stage">
-        Name: <input type="text" name="name" value="<?php  echo $name;?>">
-        <span class="error">* <?php echo $nameErr;?></span>
-        <br><br>
-        E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-        <span class="error">* <?php echo $emailErr;?></span>
-        <br><br>
-        Phone number: <input type="text" name="tele" value="<?php echo $tele;?>">
-        <span class="error"> <?php echo $teleErr;?></span>
-        <br><br>
-        Your internship's location: <input type="text" name="location" value="<?php echo $location;?>">
-        <br><br>
-	Your company's name: <input type="text" name="company" value=<?php echo $company;?>>
-	<br><br>
-        Describe your internship: <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
-        <span class="error">* <?php echo $descriptionErr;?></span>
-        <br><br>
-        <input type="submit" value="Post internship">
-    </form>
-    </div>
-    
-   </body>
+    <body>
+
+        <div class="sidepane">
+            <a href="main_page.php">Overview</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+            <a href="#">Help</a></a>
+        </div>
+
+        <div class="main">
+            <p>
+            Fill in the forms to make your internship information available to the
+            students.
+            </p>
+            <p><span class="error">* required field.</span></p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                ProjectName: <input type="text" name="naam" value="<?php  echo $naam;?>">
+                <span class="error">* <?php echo $naamErr;?></span>
+                <br><br>
+                Your internship's City: <input type="text" name="location" value="<?php echo $location;?>">
+                <span class="error">* <?php echo $locationErr;?></span>
+                <br>
+                Street: <input type="text" name="street" value="<?php echo $street;?>">
+                <span class="error"> <?php echo $streetErr;?></span>
+                StreetNumber: <input type="text" name="streetnr" value="<?php echo $streetnr;?>">
+                <span class="error"> <?php echo $streetnrErr;?></span>
+                <br><br>
+                Pay: <input type="text" name="pay" value="<?php echo $pay;?>">
+                <span class="error"> <?php echo $payErr;?></span>
+                <br><br>
+                Travel Arrangements: <input type="radio" name="travel" value="yes">Included <input type="radio" name="travel" value="no">Excluded
+                <br>
+                Notes: <input type="text" name="tnotes" value="<?php echo $tnotes;?>">
+                <span class="error"> <?php echo $tnotesErr;?></span>
+                <br><br>
+                Describe your internship: <textarea name="description" rows="5" cols="40"><?php echo $description;?></textarea>
+                <span class="error">* <?php echo $descriptionErr;?></span>
+                <br>
+                Describe the qualities you seek in a student (i.e. skillset):  <textarea name="squal" rows="5" cols="40"><?php echo $squal;?></textarea>
+                <span class="error"> <?php echo $squalErr;?></span>
+                <br>
+                Describe time restriction (when the internship is available):  <textarea name="tijdrest" rows="5" cols="40"><?php echo $tijdrest;?></textarea>
+                <span class="error"> <?php echo $tijdrestErr;?></span>
+                <br><br>
+                <input type="submit" value="Post internship">
+            </form>
+        </div>
+
+    </body>
 </html>
