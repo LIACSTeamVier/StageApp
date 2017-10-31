24380c0bc93396187520acdf3ef4d35a5af33566
informaticavia <informaticavia@users.noreply.github.com>
2017-10-31 21:22:56 +0100

    Weergeven van databasetabel werkt

--- database_table.php
+++ database_table.php
@@ -29,11 +29,23 @@ session_start();
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
+    
     $result = mysqli_query($con, "SELECT * FROM Afstudeerder") or die('Unable to run query:' . mysqli_error());
+
     echo "<table>"; // start a table tag in the HTML
     
-    while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
-        echo "<tr><td>" . $row['StudentID'] . "</td><td>" . $row['StudentNaam'] . "</td></tr>";  //$row['index'] the index here is a field name
+    // column names
+    echo "<tr><th>StudentID</th>
+              <th>StudentNaam</th>
+              <th>StuEMAIL</th>
+              <th>StudentTel</th></tr>";
+    
+    // rows of the database
+    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
+        echo "<tr><td>" . $row['StudentID'] . "</td>
+              <td>" . $row['StudentNaam'] . "</td>
+              <td>" . $row['StuEMAIL'] . "</td>
+              <td>" . $row['StudentTel'] . "</td></tr>";  //$row['index'] the index here is a field name
     }
     
     echo "</table>"; //Close the table in HTML
