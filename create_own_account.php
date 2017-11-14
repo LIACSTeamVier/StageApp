8d0dd1ec82056429f8233eca10c1654a28e4b87f
s1551396 <32844938+s1551396@users.noreply.github.com>
2017-11-14 16:33:03 +0100

    Add files via upload
    Nu beveiligd tegen injectie, en checkt voor duplicate entries.

--- create_own_account.php
+++ create_own_account.php
@@ -1,6 +1,6 @@
 <?php
 Session_start();
-$nameErr = $unameErr = $emailErr = $passErr = "";
+$nameErr = $unameErr = $emailErr = $phoneErr = $passErr = "";
 $name = $uname = $email = $phonenum = $password = "";
 $error = False;
 if (isset($_SESSION["nameErr"]))
@@ -19,6 +19,12 @@ if (isset($_SESSION["emailErr"]))
 {
     $emailErr = $_SESSION["emailErr"];
     unset($_SESSION["emailErr"]);
+    $error = True;
+}
+if (isset($_SESSION["phoneErr"]))
+{
+    $phoneErr = $_SESSION["phoneErr"];
+    unset($_SESSION["phoneErr"]);
     $error = True;
 }
 if (isset($_SESSION["passErr"]))
@@ -50,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error = True;
     } else {
         $uname = test_input($_POST["uname"]); 
-        if(strlen($uname) > 50) {
+        if(strlen($uname) > 30) {
             $unameErr = "Input too big";
             $error = True;
         }
@@ -61,12 +67,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error = True;
     } else {
         $email = test_input($_POST["email"]); 
-        if(strlen($email) > 50) {
+        if(strlen($email) > 30) {
             $emailErr = "Input too big";
             $error = True;
         }
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Invalid email format"; // W3Schools
+            $error = True;
+        }
+    }
+    
+    if (empty($_POST["phonenum"]))
+        $phonenum = NULL;
+    else {
+        $phonenum = test_input($_POST["phonenum"]); 
+        if(strlen($phonenum) != 10) {
+            $phoneErr = "Needs to be 10 digits";
+            $error = True;
+        }
+        if (!ctype_digit($_POST["phonenum"])) {
+            $phoneErr = "Digits only";
             $error = True;
         }
     }
@@ -87,8 +107,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
     
+
+    if (checkDuplicates($uname, &$unameErr, $email, &$emailErr, $phonenum, &$phoneErr))
+        $error = True;
+    
     if (!$error) {
-        attemptRegister($name, $uname, $email, $password, $passwordcheck);
+        attemptRegister($name, $uname, $email, $phonenum, $password, $passwordcheck);
     }
 }
 
@@ -99,7 +123,8 @@ function test_input($data) {
     return $data;
 }
 
-function attemptRegister($name, $uname, $email, $password, $passwordcheck) {
+function checkDuplicates($uname, &$unameErr, $email, &$emailErr, $phonenum, &$phoneErr) {
+    $error = False;
     $configs = include("config.php");
     $con = mysqli_connect($configs["host"], $configs["username"], $configs["password"], $configs["dbname"]);
     // check connection
@@ -107,19 +132,167 @@ function attemptRegister($name, $uname, $email, $password, $passwordcheck) {
         $_SESSION["regErr"] = "Failed to connect to MySQL: " . mysqli_connect_error();
     }
     else {
-        $result = mysqli_query($con, "INSERT INTO InternshipApp_Users VALUES ('$uname','Student','$name','$password');");
-        if (mysqli_error($con) != "")
-            $_SESSION["regErr"] = "Unable to run query:" . mysqli_error($con);
-        else {
-            $result = mysqli_query($con, "INSERT INTO Student VALUES ('$uname','$name','$email','$phonenum');");
-            if (mysqli_error($con) != "")
-                $_SESSION["regErr"] = "Unable to run query:" . mysqli_error($con);
-            mysqli_close($con);
+        $stmt = mysqli_prepare($con, "SELECT * FROM InternshipApp_Users i WHERE i.Identifier=?");
+        mysqli_stmt_bind_param($stmt,'s', $uname);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        if (!$result){
+            echo "database error!";
+            die ('Unable to run query:' . mysqli_error());
+        }
+        else
+            $row = mysqli_fetch_row($result);
+        if(!empty($row)) {
+            $unameErr = "Username already taken";
+            $error = True;
+        }
+        
+        $stmt = mysqli_prepare($con, "SELECT * FROM Student s WHERE s.StuEMAIL=?");
+        mysqli_stmt_bind_param($stmt,'s', $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        if (!$result){
+            echo "database error!";
+            die ('Unable to run query:' . mysqli_error());
+        }
+        else
+            $row = mysqli_fetch_row($result);
+        if(!empty($row)) {
+            $emailErr = "Email already taken";
+            $error = True;
+        }
+        
+        $stmt = mysqli_prepare($con, "SELECT * FROM Supervisor s WHERE s.SupEMAIL=?");
+        mysqli_stmt_bind_param($stmt,'s', $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        if (!$result){
+            echo "database error!";
+            die ('Unable to run query:' . mysqli_error());
+        }
+        else
+            $row = mysqli_fetch_row($result);
+        if(!empty($row)) {
+            $emailErr = "Email already taken";
+            $error = True;
+        }
+        
+        $stmt = mysqli_prepare($con, "SELECT * FROM Internship_Contact i WHERE i.IConEMAIL=?");
+        mysqli_stmt_bind_param($stmt,'s', $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        if (!$result){
+            echo "database error!";
+            die ('Unable to run query:' . mysqli_error());
+        }
+        else
+            $row = mysqli_fetch_row($result);
+        if(!empty($row)) {
+            $emailErr = "Email already taken";
+            $error = True;
+        }
+        
+        if (!is_null($phonenum)) {
+            $stmt = mysqli_prepare($con, "SELECT * FROM Student s WHERE s.StuTel=?");
+            mysqli_stmt_bind_param($stmt,'s', $phonenum);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            mysqli_stmt_close($stmt);
+            if (!$result){
+                echo "database error!";
+                die ('Unable to run query:' . mysqli_error());
+            }
+            else
+                $row = mysqli_fetch_row($result);
+            if(!empty($row)) {
+                $phoneErr = "Phonenumber already taken";
+                $error = True;
+            }
+            
+            $stmt = mysqli_prepare($con, "SELECT * FROM Supervisor s WHERE s.SupTel=?");
+            mysqli_stmt_bind_param($stmt,'s', $phonenum);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            mysqli_stmt_close($stmt);
+            if (!$result){
+                echo "database error!";
+                die ('Unable to run query:' . mysqli_error());
+            }
+            else
+                $row = mysqli_fetch_row($result);
+            if(!empty($row)) {
+                $phoneErr = "Phonenumber already taken";
+                $error = True;
+            }
+            
+            $stmt = mysqli_prepare($con, "SELECT * FROM Internship_Contact i WHERE i.IConTel=?");
+            mysqli_stmt_bind_param($stmt,'s', $phonenum);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            mysqli_stmt_close($stmt);
+            if (!$result){
+                echo "database error!";
+                die ('Unable to run query:' . mysqli_error());
+            }
+            else
+                $row = mysqli_fetch_row($result);
+            if(!empty($row)) {
+                $phoneErr = "Phonenumber already taken";
+                $error = True;
+            }
         }
     }
+    mysqli_close($con);
+    return $error;
+}
+
+function attemptRegister($name, $uname, $email, $phonenum, $password, $passwordcheck) {
+    $class = "Student";
+    insertIntoUsers($uname, $class, $name, $password);
+    insertIntoStudent($uname, $name, $email, $phonenum);
+    if (!isset($_SESSION["regErr"]))
+        $_SESSION["regErr"] = "Account created successfully!";
     // redirect to login page
     header("Location: index.php");
     exit();
+}
+
+function insertIntoUsers($uname, $class, $name, $password){
+    $configs = include("config.php");
+    $con = mysqli_connect($configs["host"], $configs["username"], $configs["password"], $configs["dbname"]);
+    if (mysqli_connect_errno()) {
+        $_SESSION["regErr"] = "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+	$stmt1 = mysqli_prepare($con, "INSERT INTO InternshipApp_Users VALUES (?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt1,'ssss', $uname, $class, $name, $password);
+	$result1 = mysqli_execute($stmt1);
+	mysqli_close($stmt1);
+	if (!$result1){
+        $_SESSION["regErr"] = "Unable to run query:" . mysqli_error($con);
+	}
+	
+    mysqli_close($con);
+}
+
+function insertIntoStudent($uname, $name, $email, $phonenum){
+    $configs = include("config.php");
+    $con = mysqli_connect($configs["host"], $configs["username"], $configs["password"], $configs["dbname"]);
+    if (mysqli_connect_errno()) {
+        $_SESSION["regErr"] = "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+	$stmt2 = mysqli_prepare($con, "INSERT INTO Student VALUES (?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt2,'ssss', $uname, $name, $email, $phonenum);
+	$result2 = mysqli_execute($stmt2);
+	mysqli_close($stmt2);
+	if (!$result2){
+        $_SESSION["regErr"] = "Unable to run query:" . mysqli_error($con);
+	}
+	
+    mysqli_close($con);
 }
 ?>
 
@@ -158,6 +331,7 @@ function attemptRegister($name, $uname, $email, $password, $passwordcheck) {
                 <span class="error">* <?php echo $emailErr;?></span>
                 <br>
                 Phone number: <input type="text" name="phonenum" value="<?php echo $phonenum;?>">
+                <span class="error"><?php echo $phoneErr;?></span>
                 <br>
                 Password: <input type="password" name="password" value="">
                 <span class="error">* <?php echo $passErr;?></span>
