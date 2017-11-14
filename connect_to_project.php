51f19ec5b6ed17774a0b6fad27954c4237114d1c
OlafRandel <olaf.randel5@gmail.com>
2017-11-14 12:17:58 +0100

    very close to completing this

--- connect_to_project.php
+++ connect_to_project.php
@@ -1,5 +1,25 @@
 <?php
-session_start();
+
+$servername = "mysql.liacs.leidenuniv.nl";
+$username = "csthesis";
+$password = "ldOIouqs";
+$dbname = "csthesis";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection 
+session_start(); 
+$id = $_GET["id"];
+
+if ($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT ProjectNaam FROM Projects";
+$result = $conn->query($sql);
+/*else {
+$sql = "INSERT INTO volbrengt (afstudeerderID, ProjectNaam)" ;// $_GET["id"]; ;// "," $_GET["project"]; ")" ;
+//$result = $conn->query($sql);
+}*/
 ?>
 
 <!DOCTYPE html>
@@ -9,16 +29,28 @@ session_start();
 <script src="sortTable.js"></script>
 </head>
 <body>
+<h1>suscribe to a project</h1>
 
-
-
-<form action="opgeven.php" method="get">
-You ID: <input type="text" name="id"><br>
-Het project: <input type="text" name="project"><br>
-<input type="submit">
+<form action="#" method="post">
+<select name="Projects">
+	<td><?php echo $counter++ ?></td>
+	<?php
+		while($row1 = mysqli_fetch_array($result1)):;
+	?>
+<option value="<?php echo $row1 [1]; ?>"><?php echo $row1 [1]; ?></option>
+<?php endwhile; ?>
+<option value="nothing"> none </option>
+</select>
+<input type="submit" name="submit" value="suscribe" />
 </form>
-
-
+<?php
+if(isset($_POST['submit'])){
+$selected_val = $_POST['Projects'];  // Storing Selected Value In Variable
+echo "We schrijven " . $id . " in in project: " . $selected_val;
+//$sql = "INSERT INTO volbrengt (afstudeerderID, ProjectNaam)" $id "," $selected_val ")" ;
+//$result = $conn->query($sql);
+}
+?>
 
 </body>
 </html> 
