1903f34b1d2f7be66b8b5221429044e4aa87d222
MartenLiacs <32845237+MartenLiacs@users.noreply.github.com>
2017-12-03 18:15:07 +0000

    cleaned admin, changed supervisor tables

--- main_page.php
+++ main_page.php
@@ -10,22 +10,20 @@ include 'sidebar_selector.php';
     }   
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!empty($_POST["progressupdate"])){
-            //if (!empty($_POST["progupdate"])){
-                if($_POST["progupdate"] != $_POST["progold"]){
-                    $pupupdate = test_input($_POST["progupdate"]);
-                    $resproj = mysqli_query($con,"SELECT * FROM Does WHERE StuID='".$_SESSION["ID"]."'");
-                    $rowproj = mysqli_fetch_array($resproj);
-                    $projname = $rowproj["ProjectName"];
-                    $stmt = mysqli_prepare($con, "UPDATE Project SET Progress=? WHERE ProjectName=?");
-                    mysqli_bind_param($stmt, 'ss', $pupupdate, $projname);
-                    mysqli_stmt_execute($stmt);
-                    $numrow = mysqli_affected_rows($con);
-                    mysqli_stmt_close($stmt);
-                    if($numrow != 1){
-                        die("Error updating progress");
-                    }
+            if($_POST["progupdate"] != $_POST["progold"]){
+                $pupupdate = test_input($_POST["progupdate"]);
+                $resproj = mysqli_query($con,"SELECT * FROM Does WHERE StuID='".$_SESSION["ID"]."'");
+                $rowproj = mysqli_fetch_array($resproj);
+                $projname = $rowproj["ProjectName"];
+                $stmt = mysqli_prepare($con, "UPDATE Project SET Progress=? WHERE ProjectName=?");
+                mysqli_bind_param($stmt, 'ss', $pupupdate, $projname);
+                mysqli_stmt_execute($stmt);
+                $numrow = mysqli_affected_rows($con);
+                mysqli_stmt_close($stmt);
+                if($numrow != 1){
+                    die("Error updating progress");
                 }
-            //}          
+            }
         }        
     }
     mysqli_close($con);
@@ -50,7 +48,8 @@ include 'sidebar_selector.php';
 				}
 				$username = $_SESSION["username"];
 				$class = $_SESSION["class"];
-				echo "<h1>LIACS Student Project Manager</h1>";
+				echo "<h1>Welcome " . "$username" ."</h1>";
+				echo "You are logged in as " . "$class" . "." ."<p>"; //TODO temp, remove line.
 
 				// After sending an e-mail
 				if (isset($_SESSION["regErr"])) {
@@ -78,18 +77,18 @@ include 'sidebar_selector.php';
 
 					// rows of the database
 					while($row = mysqli_fetch_array($result)){	 //Creates a loop to loop through results
-						$SupName1 ="";//No Supervisor";
-						$SupName2 ="";//"No Supervisor";
+						$SupName1 ="";
+						$SupName2 ="";
 						$supervisorresult = query_our_database("SELECT * FROM Supervises WHERE StuID='".$row['StuID']."' AND Accepted='1'");
-                                                while($suprow = mysqli_fetch_array($supervisorresult)){
-						    if($suprow['type'] == "First Supervisor"){
-						        $NameRow1 = mysqli_fetch_array(query_our_database("SELECT SupName FROM Supervisor WHERE SupID='".$suprow['SupID']."'"));
-							$SupName1 = $NameRow1['SupName'];						    	
-						    }
-						    if($suprow['type'] == "Second Supervisor"){
-                                                        $NameRow2 = mysqli_fetch_array(query_our_database("SELECT SupName FROM Supervisor WHERE SupID='".$suprow['SupID']."'"));
-                                                        $SupName2 = $NameRow2['SupName'];
-                                                    }
+                        while($suprow = mysqli_fetch_array($supervisorresult)){
+                            if($suprow['type'] == "First Supervisor"){
+                                $NameRow1 = mysqli_fetch_array(query_our_database("SELECT SupName FROM Supervisor WHERE SupID='".$suprow['SupID']."'"));
+                                $SupName1 = $NameRow1['SupName'];						    	
+                            }
+                            if($suprow['type'] == "Second Supervisor"){
+                                $NameRow2 = mysqli_fetch_array(query_our_database("SELECT SupName FROM Supervisor WHERE SupID='".$suprow['SupID']."'"));
+                                $SupName2 = $NameRow2['SupName'];
+                            }
 						}
 						echo "<tr><td>" . $row['StuName'] . "</td>
 									<td>" . $row['ProjectName'] . "</td>
@@ -102,60 +101,121 @@ include 'sidebar_selector.php';
 					}
 
 					echo "</table><br>"; //Close the table in HTML
-
-					//TODO: create link to supervisor history in students table
-					//Button for getting full sup history
-					//echo "<form action='supervisor_history.php' method='post'>
-					//	<input type='text' name='stuHistID' value=''>
-					//	<input type='submit' value='Get Full Supervision History For Student'>
-					//	</form>";
 				}
 				if ($class == "Supervisor") {
 					//List assigned students and their projects
-					$result = query_our_database("SELECT Student.StuID, Student.StuName, Student.StuEMAIL, Student.StuTel, Does.ProjectName, Project.Progress FROM Student LEFT JOIN Does ON Student.StuID=Does.StuID LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE Project.SupID='".$_SESSION["ID"]."'");
-				
-					echo "<h3>Student overview</h3>
-								<table>"; // start a table tag in the HTML
-				
-					// column names
-					echo "<tr><th>Name</th>
-								<th>Project</th>
-								<th>Progress</th>
-								<th>Student ID</th>
-								<th>E-mail</th>
-								<th>Telephone</th>
-								</tr>";
+					//$result = query_our_database("SELECT Student.StuID, Student.StuName, Student.StuEMAIL, Student.StuTel, Does.ProjectName, Project.Progress FROM Student LEFT JOIN Does ON Student.StuID=Does.StuID LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE Project.SupID='".$_SESSION["ID"]."'");
+                    $result = query_our_database("SELECT Supervises.StuID, StuName, StuEMAIL, StuTel, type, Supervises.SupID, Supervises.Accepted as SupAccepted, Does.ProjectName, Does.Accepted as ProjectAccepted, Description, Progress FROM Student LEFT JOIN Supervises ON Student.StuID=Supervises.StuID LEFT JOIN Does ON Student.StuID=Does.StuID LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE Supervises.SupID='".$_SESSION["ID"]."' AND Supervises.Accepted='1' ORDER BY StuID");
+                    if(mysqli_num_rows($result) > 0){
+                        echo "<h3>Student overview</h3>
+                                    <table class=\"list\">"; // start a table tag in the HTML
+                    
+                        // column names
+                        echo "<tr><th>Name</th>
+                                <th>First Supervisor</th>
+                                <th>Second Supervisor</th>
+                                <th>Project Name and Description</th>
+                                <th>Project Accepted</th>
+                                <th>Progress</th>
+                                <th>Student ID</th>
+                                <th>E-mail</th>
+                                <th>Phone Number</th>
+                                </tr>";
 
-					// rows of the database
-					while($row = mysqli_fetch_array($result)){	 //Creates a loop to loop through results
-						echo "<tr><td>" . $row['StuName'] . "</td>
-									<td>" . $row['ProjectName'] . "</td>
-									<td>" . $row['Progress'] . "</td>
-									<td>" . $row['StuID'] . "</td>
-									<td>" . $row['StuEMAIL'] . "</td>
-									<td>" . $row['StuTel'] . "</td></tr>";	//$row['index'] the index here is a field name
-					}
+                        // rows of the database
+                        $prevStuID = "";
+                        while($row = mysqli_fetch_array($result)){	 //Creates a loop to loop through results
+                            $SupName1 ="";
+                            $SupName2 ="";
+                            if (!($prevStuID == $row['StuID'])){//prevent duplicate rows
+                                $supsresult = query_our_database("SELECT SupName, Supervises.SupID, Accepted, type FROM Supervises LEFT JOIN Supervisor ON Supervises.SupID=Supervisor.SupID WHERE StuID='".$row['StuID']."' AND (Accepted='0' OR Accepted='1')");
+                                while($suprow = mysqli_fetch_array($supsresult)){
+                                    if($suprow['type'] == "First Supervisor"){
+                                        if($suprow['SupID'] == $_SESSION["ID"])
+                                            $SupName1 = "You";
+                                        else{
+                                            if($suprow['Accepted']=='1')
+                                                $SupName1 = $suprow['SupName'];
+                                            else
+                                                $SupName1 = $suprow['SupName']."(not accepted yet)";
+                                        }
+                                    }
+                                    if($suprow['type'] == "Second Supervisor"){
+                                        if($suprow['SupID'] == $_SESSION["ID"])
+                                            $SupName2 = "You";
+                                        else{
+                                            if($suprow['Accepted']=='1')
+                                                $SupName2 = $suprow['SupName'];
+                                            else
+                                                $SupName2 = $suprow['SupName']."(not accepted yet)";
+                                        }
+                                    }
+                                }
+                                $pAccepted = "";
+                                $prog = "";
+                                if ($row['ProjectAccepted'] == '1'){
+                                    $pAccepted = "Yes";
+                                    $prog = $row['Progress'];
+                                }
+                                else if ($row['ProjectAccepted'] == '0'){
+                                    $pAccepted = "No";
+                                }
+                                echo "<tr><td>" . $row['StuName'] . "</td>
+                                            <td>" . $SupName1 . "</td>
+                                            <td>" . $SupName2 . "</td>
+                                            <td width='40%'><b>" . $row['ProjectName'] . "</b><p style='margin-left: 5px'>" . $row['Description'] . "</p></td>
+                                            <td>$pAccepted</td>
+                                            <td>$prog</td>
+                                            <td>" . $row['StuID'] . "</td>
+                                            <td>" . $row['StuEMAIL'] . "</td>
+                                            <td>" . $row['StuTel'] . "</td></tr>";	//$row['index'] the index here is a field name
+                            }
+                            $prevStuID = $row['StuID'];
+                        }
 
-					echo "</table><br>"; //Close the table in HTML				
-					
-					//Button for creating a project (TODO: move to projects page)
-					echo "<form action='project.php' method='head'>
-									<input type='submit' value='Create a university project'>
-								</form>";
+                        echo "</table><br>"; //Close the table in HTML	
+                    }
+                    $result = query_our_database("SELECT Does.ProjectName, Description, Student.StuID, StuName, StuEMAIL, StuTel, Progress FROM Student LEFT JOIN Does ON Student.StuID=Does.StuID LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE Project.SupID='".$_SESSION["ID"]."'");
+                    
+                    if(mysqli_num_rows($result) > 0){
+                        echo "<h3>Project Overview</h3>
+                                    <table class=\"list\">"; // start a table tag in the HTML
+                        
+                        // column names
+                        echo "<tr><th>Name and Description</th>
+                                <th>Student Name</th>
+                                <th>Student E-mail</th>
+                                <th>Student Phone Number</th>
+                                <th>Progress</th>
+                                </tr>";
+                        
+                        
+                        // rows of the database
+                        while($row = mysqli_fetch_array($result)){	 //Creates a loop to loop through results
+                            echo "<tr><td width='40%'><b>" . $row['ProjectName'] . "</b><p style='margin-left: 5px'>" . $row['Description'] . "</p></td>
+                                        <td>" . $row['StuName'] . "</td>
+                                        <td>" . $row['StuEMAIL'] . "</td>
+                                        <td>" . $row['StuTel'] . "</td>
+                                        <td>" . $row['Progress'] . "</td></tr>";	//$row['index'] the index here is a field name
+                        }
+
+                        
+                        echo "</table><br>"; //Close the table in HTML	
+                    }
 				}
-				if ($class == "Internship Contact") {
+                if ($class == "Internship Contact") {
 					//TODO here: add information on internship + participating students
-				}
-				if ($class == "Student") {
-					//Show your project and supervisors
-					
-					$result = query_our_database("SELECT Does.ProjectName, Project.Description, Project.Progress, Project.Time, Project.Internship, Project.SupID, Project.IConID FROM Does LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE StuID='".$_SESSION["ID"]."'");
-					$row = mysqli_fetch_array($result);
-                    
-                    
-					
-					echo "<h2>My project</h2>";
-					if ($row['ProjectName'] != "") {
+                }
+                if ($class == "Student") {
+                    //Show your project and supervisors
+
+                    $result = query_our_database("SELECT Does.ProjectName, Project.Description, Project.Progress, Project.Time, Project.Internship, Project.SupID, Project.IConID FROM Does LEFT JOIN Project ON Does.ProjectName=Project.ProjectName WHERE StuID='".$_SESSION["ID"]."'");
+                    $row = mysqli_fetch_array($result);
+
+
+
+                    echo "<h2>My project</h2>";
+                    if ($row['ProjectName'] != "") {
                         $type = $row["Internship"];//1 is internship
                         if($type == "1"){
                             $result2 = query_our_database("SELECT * FROM Internship_of WHERE ProjectName='".$row["ProjectName"]."'");
@@ -167,9 +227,8 @@ include 'sidebar_selector.php';
                             $result2 = query_our_database("SELECT * FROM Supervisor WHERE SupID='".$row["SupID"]."'");
                             $rowcontactinfo = mysqli_fetch_array($result2);
                         }
-						//echo "<h3>Title: " . $row['ProjectName']. "</h3>";
-						//echo "<p style='margin-left: 5px'>". $row['Description'] . "<p>";
-						echo "<table class=\"list\">
+                        
+                        echo "<table class=\"list\">
                             <tr>";
                         if($type == "1"){
                             echo "<th>Name and Description</th><th>Time</th><th>Project Owner Name</th><th>Owner Email</th><th>Owner Phone Number</th><th>Type</th><th>Company Name</th><th>City</th><th>Street</th><th>Nr</th><th>Travel</th><th>Pay</th>
@@ -216,18 +275,14 @@ include 'sidebar_selector.php';
                             <input type=\"submit\" name=\"progressupdate\" value=\"Update Your Progress\">
                         </form>";					
                         
-					}
-					else {
-						echo "You currently have no project.<br>";
-					}
-					
-                    /*    if ($row['Progress'] != "") 
-							echo "<h3>Progress: " . $row['Progress'] . "</h3>";
-						else*/
-                   //$result = query_our_database("SELECT Supervises.type, Supervisor.SupName, Supervises.Accepted, Supervisor.SupEMAIL, Supervisor.SupTel FROM Supervises LEFT JOIN Supervisor ON Supervises.SupID=Supervisor.SupID WHERE StuID='".$_SESSION["ID"]."' ORDER BY type"); //TODO: when "active" is in database, filter on it
-					$result = query_our_database("SELECT SupID, Accepted FROM Supervises WHERE StuID='".$_SESSION["ID"]."' AND type='First SuperVisor'");
-					
-					//first supervisor
+                    }
+                    else {
+                        echo "You currently have no project.<br>";
+                    }
+
+                    $result = query_our_database("SELECT SupID, Accepted FROM Supervises WHERE StuID='".$_SESSION["ID"]."' AND type='First SuperVisor'");
+
+                    //first supervisor
                     $found = false;
                     while ($row = mysqli_fetch_array($result)){
                         if ($row['Accepted'] == "0" || $row['Accepted'] == "1"){
@@ -246,29 +301,11 @@ include 'sidebar_selector.php';
                     }
                     if (!$found)
                         echo "<h3>First supervisor: -</h3>";
-					/*$row = mysqli_fetch_array($result); //go through the two (active) rows
-					if ($row['type'] == "First Supervisor") {
-						echo "<h3>First supervisor: " . $row['SupName'];
-					}
-					else {
-						echo "<h3>First supervisor: -";
-					}
-					
-					if ($row['Accepted'] == 1 || $row['Accepted'] == NULL) {
-						echo "</h3>";
-					}
-					else {
-						echo " (not confirmed yet)</h3>";
-					}
-					
-					if ($row['SupName'] != NULL) {
-						echo "<p style='margin-left: 5px'>E-mail: " . $row['SupEMAIL'] . "</p>";
-						echo "<p style='margin-left: 5px'>Telephone: " . $row['SupTel'] . "</p>";
-					}*/
-					
+
+
                     $result = query_our_database("SELECT SupID, Accepted FROM Supervises WHERE StuID='".$_SESSION["ID"]."' AND type='Second SuperVisor'");
-					
-					//second supervisor
+
+                    //second supervisor
                     $found = false;
                     while ($row = mysqli_fetch_array($result)){
                         if ($row['Accepted'] == "0" || $row['Accepted'] == "1"){
@@ -288,28 +325,6 @@ include 'sidebar_selector.php';
                     if (!$found)
                         echo "<h3>Second supervisor: -</h3>";
                     
-					/*
-					//second supervisor
-					$row = mysqli_fetch_array($result); //go through the two (active) rows
-					if ($row['type'] == "Second Supervisor") {
-						echo "<h3>Second supervisor: " . $row['SupName'];
-					}
-					else {
-						echo "<h3>Second supervisor: -";
-					}
-					
-					if ($row['Accepted'] == 1 || $row['Accepted'] == NULL) {
-						echo "</h3>";
-					}
-					else {
-						echo " (not confirmed yet)</h3>";
-					}
-					
-					if ($row['SupName'] != NULL) {
-						echo "<p style='margin-left: 5px'>E-mail: " . $row['SupEMAIL'] . "</p>";
-						echo "<p style='margin-left: 5px'>Telephone: " . $row['SupTel'] . "</p>";
-					}
-					*/
 				}
 			
 			?>
@@ -321,3 +336,4 @@ include 'sidebar_selector.php';
 
 	</body>
 </html> 
+
